3c6ca0ad9a8499336f34ca74ac620ecdd4b3d568
aquzohr <kampee.expertly01@gmail.com>
2018-08-24 12:21:42 +0700

    add params

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :courses
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
